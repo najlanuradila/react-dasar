be9461d2ffef74d246f184fc7059476a059ee1c8
najlanuradila <najlaadila1808@gmail.com>
2025-01-07 15:39:01 +0700

    Conditional, Collection Component, Pure Component, Event Handler, Event Object, dan Event Propagation

--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -3,12 +3,26 @@ import { createRoot } from 'react-dom/client'
 import HelloWorld from './HelloWorld.jsx'
 import Container from './Container.jsx'
 import TodoList from '../todolist/TodoList.jsx'
+import Table from '../table/Table.jsx'
+import AlertButton from '../button/AlertButton.jsx'
+import MyButton from '../button/MyButton.jsx'
+import Toolbar from '../button/Toolbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Container>
         <HelloWorld />
         <TodoList/>
+        <Table/>
+
+
+        <AlertButton text="Click Me" message="You Click Me!"/>
+        <MyButton text="Smash Me" onSmash={() => alert("You Smash Me!")}/> 
+
+        <Toolbar onClick={(e) => {
+          e.stopPropagation();
+          alert("You Click Toolbar")
+        }}/>
     </Container>
   </StrictMode>,
 )
--- src/todolist/TodoList.jsx
+++ src/todolist/TodoList.jsx
@@ -1,12 +1,34 @@
 import Todo from "./Todo"
 
-export default function() {
+export default function TodoList() {
+    const data = [
+        {
+            id : 0,
+            text : "Learn HTML",
+            isCompleted : true
+        },
+        {
+            id : 1,
+            text : "Learn CSS",
+            isCompleted : true
+        },
+        {
+            id : 2,
+            text : "Learn JavaScript",
+            isCompleted : true
+        },
+        {
+            id : 3,
+            text : "Learn ReactJS",
+            isCompleted : false
+        }
+    ]
+
     return (
         <ul>
-            <Todo isCompleted={true} text="Learn HTML" isDeleted={true}/>
-            <Todo isCompleted={true} text="Learn CSS"/>
-            <Todo isCompleted={true} text="Learn JavaScript"/>
-            <Todo isCompleted={false} text="Learn ReactJS"/>
+            {data.map((todo) => (
+                <Todo key={todo.id} {...todo}/>
+            ))}
         </ul>
     )
 }
